04569628e04b353bec4a0b2c8ff4bdf7ab003134
bima bagaskhoro <bima.bagaskhoro@gmail.com>
2026-02-02 01:20:17 +0700

    EVAL_IMAGE = "/eval_images"

--- trainer/constants.py
+++ trainer/constants.py
@@ -11,6 +11,7 @@ CACHE_CLEANER_DOCKER_IMAGE = "diagonalge/cache-cleaner:latest"
 IMAGE_TASKS_HF_SUBFOLDER_PATH = "checkpoints"
 VECTOR_URL = "http://localhost:8688"  # Vector http_server for logging
 IMAGE_BUILD_RETRIES = 3
+EVAL_IMAGE = "/eval_images"
 
 # Dynamic resource allocation based on GPU count
 # For 8xH100 with 1440GB RAM and 252 CPUs
